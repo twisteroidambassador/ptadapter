4fe739ff044d674ed64830a51a062d7fc8d5588c
twisteroidambassador <twisteroidambassador@users.noreply.github.com>
2016-03-25 21:47:55 -0400

    Change __init__.py so that PluggableTransportClientTCPAdapter is now an optional import.

--- pluggabletransportadapter/__init__.py
+++ pluggabletransportadapter/__init__.py
@@ -1,8 +1,13 @@
 from .baseserverclient import PluggableTransportBaseAdapter, \
     PluggableTransportServerAdapter, PluggableTransportClientSOCKSAdapter
 
-from .tcpclient import PluggableTransportClientTCPAdapter
-
 __ALL__ = ["PluggableTransportServerAdapter", \
-           "PluggableTransportClientSOCKSAdapter", \
-           "PluggableTransportClientTCPAdapter"]
+        "PluggableTransportClientSOCKSAdapter"]
+
+try:
+    from .tcpclient import PluggableTransportClientTCPAdapter
+except ImportError:
+    # If tcpclient.py or any of its dependencies are missing, silently skip import
+    pass
+else:
+    __ALL__.append("PluggableTransportClientTCPAdapter")

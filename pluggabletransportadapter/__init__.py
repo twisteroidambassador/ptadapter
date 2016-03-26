e7fe1df1931ee6c3181ec3b2f55659b2f5d89fde
twisteroidambassador <twisteroidambassador@users.noreply.github.com>
2016-03-25 21:41:40 -0400

    Put PluggableTransportClientTCPAdapter back into the package.
    This reverts commit 88c50d9aba154465ef739b53474a387a6c12bbcb.

--- pluggabletransportadapter/__init__.py
+++ pluggabletransportadapter/__init__.py
@@ -1,5 +1,8 @@
 from .baseserverclient import PluggableTransportBaseAdapter, \
     PluggableTransportServerAdapter, PluggableTransportClientSOCKSAdapter
 
+from .tcpclient import PluggableTransportClientTCPAdapter
+
 __ALL__ = ["PluggableTransportServerAdapter", \
-           "PluggableTransportClientSOCKSAdapter"]
+           "PluggableTransportClientSOCKSAdapter", \
+           "PluggableTransportClientTCPAdapter"]

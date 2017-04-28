64a90fb272224a331f69d1c59e3357a9687c1075
twisteroidambassador <twisteroidambassador@users.noreply.github.com>
2017-04-28 15:42:35 +0800

    Shortened package name. Rewritten client script.
    Server script is probably unusable right now. The plan is to consolidate
    server and client into the same script.

--- standalone_client.py
+++ standalone_client.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-'''Standalone client for obfs4proxy and other pluggable transports.
+"""Client end of a pluggable transport tunnel.
 
 This script takes a pluggable transport binary and run it as a standalone
 client, accepting plaintext traffic, scrambles it and forwards obfuscated
-traffic.'''
+traffic."""
 
 import logging
 import argparse
@@ -12,41 +12,61 @@ import configparser
 import signal, sys
 import asyncio
 
-import pluggabletransportadapter
-#from pluggabletransportadapter import PluggableTransportClientTCPAdapter
+import ptadapter
 
 def main_cli():
-    # Argument Parsing
-    parser = argparse.ArgumentParser(description="Run a Tor pluggable "
-              "transport as standalone client.")
-    
-    parser.add_argument("configfile", type=argparse.FileType("r"), help=
-            "Configuration file. See the example config file for details.")
-    parser.add_argument("--verbose", "-v", action="count", help="Increase " 
-        "verbosity level. Specify once to see logging.INFO, twice to see "
-        "logging.DEBUG.")
+    parser = argparse.ArgumentParser(description='Run a Tor pluggable '
+              'transport as standalone server or client.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--server', '-S', action='store_true', help='Run as the '
+            'server end of a tunnel.')
+    group.add_argument('--client', '-C', action='store_true', help='Run as the '
+            'client end of a tunnel.')
+    parser.add_argument('configfile', type=argparse.FileType('r'), help=
+            'Configuration file. See the example config file for details.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--verbose', '-v', action='count', help='Increase ' 
+            'verbosity level. Specify once to see logging.INFO, twice to see '
+            'logging.DEBUG. Cannot be used with --quiet.')
+    group.add_argument('--quiet', '-q', action='store_true', help='Suppress '
+            'all logger output to STDERR. Cannot be used with --verbose.')
+    parser.add_argument('--access-log', '-l', help='Save access log to the '
+            'specified file.')
     
     args = parser.parse_args()
     
     # Logging
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    
-    logconsole = logging.StreamHandler()
-    logconsoleformatter = logging.Formatter('[%(asctime)s] %(name)-6s '
-            '%(levelname)-8s %(message)s')
-    logconsole.setFormatter(logconsoleformatter)
     if args.verbose is None:
-        logconsole.setLevel(logging.WARNING)
+        root_loglevel = logging.WARNING
     elif args.verbose == 1:
-        logconsole.setLevel(logging.INFO)
+        root_loglevel = logging.INFO
     else:
-        logconsole.setLevel(logging.DEBUG)
-
+        root_loglevel = logging.DEBUG
+    logger = logging.getLogger()
+    logger.setLevel(root_loglevel)
+    
+    if not args.quiet:
+        logconsole = logging.StreamHandler()
+        #logconsoleformatter = logging.Formatter('[%(asctime)s] %(name)-6s '
+        #        '%(levelname)-8s %(message)s')
+        logconsoleformatter = logging.Formatter('[%(asctime)s] %(name)s '
+                '%(levelname)s %(message)s')
+        logconsole.setFormatter(logconsoleformatter)
+        logconsole.setLevel(root_loglevel)
+    else:
+        logconsole = logging.NullHandler()
     logger.addHandler(logconsole)
     
-    logger.debug("Verbosity level set")
-    logger.debug("Arguments:")
+    if args.access_log is not None:
+        access_log_handler = logging.FileHandler(args.access_log)
+        access_log_formatter = logging.Formatter('%(asctime)s %(message)s')
+        access_log_handler.setFormatter(access_log_formatter)
+        access_log_handler.setLevel(logging.WARNING)
+        access_logger = logging.getLogger('ptadapter.access')
+        access_logger.addHandler(access_log_handler)
+    
+    logger.debug('Logging options set')
+    logger.debug('Arguments:')
     logger.debug(args)
     
     # Read config file
@@ -54,58 +74,64 @@ def main_cli():
     config.read_file(args.configfile)
     args.configfile.close()
     
-    logger.info("Read config file")
+    logger.info('Read config file')
     
+    loop = ptadapter.get_event_loop()
+    asyncio.set_event_loop(loop)
+    
+    if args.client:
+        pt = get_client(config, loop)
+    else:
+        pt = get_server(config, loop)
+    
+    pt.start()
+    
+    ptadapter.windows_async_signal_helper(loop)
+    
+    # Wait until PT terminates, or terminate on Ctrl+C / SIGTERM
+    try:
+        signal.signal(signal.SIGTERM, sigterm_handler)
+        pt.wait()
+        logger.warning('PT exited unexpectedly')
+    except (KeyboardInterrupt, SystemExit) as e:
+        logger.info('Received {}'.format(repr(e)))
+        pt.stop()
+        loop.run_forever()
+    finally:
+        logger.info('Terminating')
+        loop.close()
+
+def get_client(config, loop):
     # Build client configuration
-    ptexec = config["common"]["exec"]
-    statedir = config["common"]["statedir"]
-    if config.has_option("common", "upstream-proxy"):
-        upstream_proxy = config["common"]["upstream-proxy"]
+    ptexec = config['common']['exec']
+    statedir = config['common']['statedir']
+    if config.has_option('common', 'upstream-proxy'):
+        upstream_proxy = config['common']['upstream-proxy']
     else:
         upstream_proxy = None
     
     transports = {}
-    for s, t in config.items("transports"):
+    for s, t in config.items('transports'):
         tr = {
-            "listen_host": config[s]["listen-addr"],
-            "listen_port": int(config[s]["listen-port"]),
-            "remote_host": config[s]["server-addr"],
-            "remote_port": int(config[s]["server-port"])
+            'listen_host': config[s]['listen-addr'],
+            'listen_port': int(config[s]['listen-port']),
+            'remote_host': config[s]['server-addr'],
+            'remote_port': int(config[s]['server-port'])
             }
-        opt = {o[8:]:v for (o,v) in config.items(s) if o[:8] == "options-"}
-        if opt: tr["options"] = opt
+        opt = {o[8:]:v for (o,v) in config.items(s) if o.startswith('options-')}
+        if opt: tr['options'] = opt
         
         if not t in transports:
             transports[t] = {}
         transports[t][s] = tr
     
-    logger.debug("Transports:")
-    logger.debug(transports)
-    
-    # Start PT executable
-    loop = pluggabletransportadapter.get_event_loop()
-    asyncio.set_event_loop(loop)
-    client = pluggabletransportadapter.PTClientListeningAdapter(loop, ptexec, statedir, transports, upstream_proxy)
-    client.start()
-    
-    pluggabletransportadapter.windows_async_signal_helper(loop)
-    
-    # Wait until PT terminates, or terminate on Ctrl+C / SIGTERM
-    try:
-        signal.signal(signal.SIGTERM, sigterm_handler)
-        client.wait()
-        logger.warning('PT exited unexpectedly')
-    except (KeyboardInterrupt, SystemExit) as e:
-        logger.info("Received {}".format(repr(e)))
-        client.stop()
-        loop.run_forever()
-    finally:
-        logger.info("Terminating")
-        loop.close()
-        
+    return ptadapter.PTClientListeningAdapter(loop, ptexec, statedir, transports, upstream_proxy)
+
+def get_server(config):
+    raise NotImplementedError
     
 def sigterm_handler(signal, frame):
     sys.exit(0)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main_cli()

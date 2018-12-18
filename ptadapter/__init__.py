65a51237c5a07e6a1dd869f8468fcda14636809b
twisteroidambassador <twisteroidambassador@users.noreply.github.com>
2018-12-18 22:30:16 +0800

    Bump version.

--- ptadapter/__init__.py
+++ ptadapter/__init__.py
@@ -8,6 +8,6 @@ from .adapters import *
 
 __all__ = adapters.__all__
 
-__version__ = '3.0.0b1'
+__version__ = '3.0.0rc1'
 # Note: since ptadapter does not have any 3rd-party dependencies,
 # it should be safe for setup.py to import this.
